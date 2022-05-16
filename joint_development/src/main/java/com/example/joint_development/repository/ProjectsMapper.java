1ab94662afc2d76b2792b71c0020be8c9a4ba7ae
shimon0 <shimon.panipani36s@gmail.com>
2022-05-16 11:10:58 +0900

    プロジェクト概要一覧取得

--- joint_development/src/main/java/com/example/joint_development/repository/ProjectsMapper.java
+++ joint_development/src/main/java/com/example/joint_development/repository/ProjectsMapper.java
@@ -1,5 +1,7 @@
 package com.example.joint_development.repository;
 
+import java.util.List;
+
 import com.example.joint_development.domain.Projects;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -9,5 +11,8 @@ public interface ProjectsMapper {
     
     /** プロジェクト作成 */
     public int makeProject(Projects projects);
+
+    /** プロジェクト概要一覧取得 */
+    public List<Projects> findProjectList();
 
 }
